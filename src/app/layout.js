740f16f5bbe588ce853e33f662a9a15bfb65f79d
wenlx <wenlx@dtjoy.com>
2024-10-31 16:08:08 +0800

    add google analystics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'; 
 import Header from './components/Header'; 
 import Footer from './components/Footer';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Free AI Tools',
@@ -12,8 +13,21 @@ const Layout = ({ children }) => {
     <html lang="en">
       <body>
         <Header />
-        <main>{children}</main> {/* 渲染子页面内容 */}
+        <main>{children}</main>
         <Footer />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YE2FH6DW8Y"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YE2FH6DW8Y');
+          `}
+        </Script>
       </body>
     </html>
   );
